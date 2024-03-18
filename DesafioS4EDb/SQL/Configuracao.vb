e594d5067ee7e40cdbbf3e0290e08ad3525c972f
Italo Douglas Vasconcelos de Alencar <italodouglas.dev@gmail.com>
2024-03-18 18:19:26 -0300

    Update202403181819

--- DesafioS4EDb/SQL/Configuracao.vb
+++ DesafioS4EDb/SQL/Configuracao.vb
@@ -2,9 +2,7 @@
 
     Friend Class Configuracao
 
-
         Friend Const STRING_CONEXAO As String = "Initial Catalog=NOME_BANCO; User ID=NOME_USUARIO; Password=SENHA_USUARIO; Data Source=NOME_SERVIDOR"
-
 
     End Class
 
